8b3125611cce7710677eeed205344096937deed6
Ajay <ajay.nellori@gmail.com>
2024-02-22 19:18:02 +0530

    Initial Commit

---
+++ SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Venkata Ajay Sai Nellori on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
